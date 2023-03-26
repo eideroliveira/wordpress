45aa8bad216387cdef0fa72af95a82adfc95a670
Eider Oliveira <eider.oliveira@gmail.com>
2023-03-25 20:47:26 -0700

    Support for User Meta endpoint

--- users.go
+++ users.go
@@ -11,6 +11,8 @@ type AvatarURLS struct {
 	Size96 string `json:"96,omitempty"`
 }
 type User struct {
+	collection *UsersCollection `json:"-,omitempty"`
+
 	ID                int                    `json:"id,omitempty"`
 	AvatarURL         string                 `json:"avatar_url,omitempty"`
 	AvatarURLs        AvatarURLS             `json:"avatar_urls,omitempty"`
@@ -36,6 +38,22 @@ type UsersCollection struct {
 	url    string
 }
 
+func (entity *User) setCollection(col *UsersCollection) {
+	entity.collection = col
+}
+func (entity *User) Meta() *MetaCollection {
+	if entity.collection == nil {
+		// missing user.collection parent. Probably User struct was initialized manually.
+		_warning("Missing parent post collection")
+		return nil
+	}
+	return &MetaCollection{
+		client:     entity.collection.client,
+		parent:     entity,
+		parentType: CollectionUsers,
+		url:        fmt.Sprintf("%v/%v/%v", entity.collection.url, entity.ID, CollectionMeta),
+	}
+}
 func (col *UsersCollection) Me(params interface{}) (*User, *http.Response, []byte, error) {
 	url := fmt.Sprintf("%v/me", col.url)
 	var user User
@@ -45,28 +63,48 @@ func (col *UsersCollection) Me(params interface{}) (*User, *http.Response, []byt
 func (col *UsersCollection) List(params interface{}) ([]User, *http.Response, []byte, error) {
 	var users []User
 	resp, body, err := col.client.List(col.url, params, &users)
+	// set collection object for each entity which has sub-collection
+	for _, u := range users {
+		u.setCollection(col)
+	}
+
 	return users, resp, body, err
 }
 func (col *UsersCollection) Create(new *User) (*User, *http.Response, []byte, error) {
 	var created User
 	resp, body, err := col.client.Create(col.url, new, &created)
+
+	created.setCollection(col)
+
 	return &created, resp, body, err
 }
 func (col *UsersCollection) Get(id int, params interface{}) (*User, *http.Response, []byte, error) {
 	var entity User
 	entityURL := fmt.Sprintf("%v/%v", col.url, id)
 	resp, body, err := col.client.Get(entityURL, params, &entity)
+
+	// set collection object for each entity which has sub-collection
+	entity.setCollection(col)
+
 	return &entity, resp, body, err
 }
 func (col *UsersCollection) Update(id int, post *User) (*User, *http.Response, []byte, error) {
 	var updated User
 	entityURL := fmt.Sprintf("%v/%v", col.url, id)
 	resp, body, err := col.client.Update(entityURL, post, &updated)
+
+	// set collection object for each entity which has sub-collection
+	updated.setCollection(col)
+
 	return &updated, resp, body, err
 }
 func (col *UsersCollection) Delete(id int, params interface{}) (*User, *http.Response, []byte, error) {
 	var deleted User
 	entityURL := fmt.Sprintf("%v/%v", col.url, id)
 	resp, body, err := col.client.Delete(entityURL, params, &deleted)
+
+	// set collection object for each entity which has sub-collection
+	deleted.setCollection(col)
+
 	return &deleted, resp, body, err
 }
